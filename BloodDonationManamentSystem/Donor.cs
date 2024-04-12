28b41b4f391cfb328c467e48ac198502a964cb77
Nethsarani <nethsaraniprasangika09@gmail.com>
2024-04-12 14:46:07 +0530

    c

---
+++ BloodDonationManamentSystem/Donor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xaml.Schema;
+
+namespace BloodDonationManamentSystem
+{
+    internal class Donor
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string NIC { get; set; }
+        public DateTime DOB { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string BloodType { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
