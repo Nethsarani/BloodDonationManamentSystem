d0a0612d32f55c1f78c3c3c14c845e33934f8dcf
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-10 21:15:53 +0530

    not

---
+++ BloodDonationManamentSystem/Properties/MobileDonations.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BloodDonationManamentSystem.Properties
+{
+    /// <summary>
+    /// Interaction logic for MobileDonations.xaml
+    /// </summary>
+    public partial class MobileDonations : Page
+    {
+        public MobileDonations()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
