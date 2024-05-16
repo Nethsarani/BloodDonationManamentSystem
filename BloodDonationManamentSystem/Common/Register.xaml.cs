ab77812b91f313b6ff79eb424fc5741375978891
Nethsarani <129503280+Nethsarani@users.noreply.github.com>
2024-05-16 12:02:19 +0530

    update BloodDonationManamentSystem/Common/Register.xaml.cs

--- BloodDonationManamentSystem/Common/Register.xaml.cs
+++ BloodDonationManamentSystem/Common/Register.xaml.cs
@@ -75,6 +75,7 @@ namespace BloodDonationManamentSystem
 user.hospital= dB.getHospital(dB.IDCheck("Hospital", "Username", hospital.Username));
                 user.Name = txtName.Text;
                 user.Email = txtEmail.Text;
+                user.NIC="00000000";
                 user.Position = "Admin";
                 user.ContactNo = txtContact.Text;
                 user.UserName = txtUsername.Text;
